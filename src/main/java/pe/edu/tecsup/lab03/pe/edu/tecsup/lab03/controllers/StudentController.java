15f7821960c65cfc0832ae5aed22c40be9ecb809
David Carhuaz <david.carhuaz@tecsup.edu.pe>
2025-09-01 17:32:12 -0500

    Otro commit de David Carhuaz :Reparando errores

--- src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -2,4 +2,7 @@ package pe.edu.tecsup.lab03.pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
     //David Carhuaz
+    public void init(){
+        System.out.println("Ni¿o retorna nada");
+    }
 }
